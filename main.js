0e03609c49f9be9cc3fc8e638aa9239774147e4f
Leonardo Vettulo <leonardovettulo@gmail.com>
2019-09-12 20:20:15 -0300

    Crear minas aleatoriamente y asignar cantidad de minas vecinas a cada caja

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var cantCol = 10; //Cantidad total de columnas
 var cantFil = 10; //Cantidad total de filas
-var cantMinas = 5; //Cantidad total de minas
+var cantMinas = 10; //Cantidad total de minas
 var tablero = [[]]; //Array de objetos
 var divPrincipal = document.getElementById("principal");
 //Clase para guardar las coordenadas de cada caja(casillero) del tablero
@@ -19,7 +19,7 @@ function crearTablero(cantCol, cantFil) {
     for (var fil = 0; fil < cantFil; fil++) {
         tablero[fil] = [];
         for (var col = 0; col < cantCol; col++) {
-            tablero[fil][col] = new Caja(fil, col, "0");
+            tablero[fil][col] = new Caja(fil, col, 0);
         }
     }
 }
@@ -35,6 +35,46 @@ function previaTablero() {
     }
     divPrincipal.innerHTML = imprimir;
 }
+//Función para crear minas aleatoreamente en filas y columnas
+//Recibe como argumento la cantidad de minas, que puede ser elegido desde la variable global cantMinas
+function crearMinas(cantMinas) {
+    while (cantMinas > 0) {
+        var filRand = Math.floor(Math.random() * cantFil);
+        var colRand = Math.floor(Math.random() * cantCol);
+        if (tablero[filRand][colRand].status != -1) {
+            tablero[filRand][colRand].status = -1;
+            cantMinas--;
+            //console.log(cantMinas);
+        }
+    }
+}
+//Función para asignar el número de minas vecinas
+//Para cada caja, verificamos si es una mina, y si no verificamos todas las vecinas
+//Si la vecina no es una mina o no esta fuera de tablero adicionamos en 1 el contador de status
+function asignarNumero() {
+    for (var fil = 0; fil < cantFil; fil++) {
+        for (var col = 0; col < cantCol; col++) {
+            if (tablero[fil][col].status === -1) {
+                for (var offCol = -1; offCol <= 1; offCol++) {
+                    for (var offFil = -1; offFil <= 1; offFil++) {
+                        //console.log(fil + offFil, col + offCol);
+                        if (
+                        //Debo verificar que no quede fuera del tablero y que no sea una mina (-1)
+                        fil + offFil >= 0 &&
+                            fil + offFil < cantFil &&
+                            col + offCol >= 0 &&
+                            col + offCol < cantCol &&
+                            tablero[fil + offFil][col + offCol].status !== -1) {
+                            tablero[fil + offFil][col + offCol].status++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 crearTablero(cantCol, cantFil);
+crearMinas(cantMinas);
+asignarNumero();
 previaTablero();
-console.log(tablero);
+//console.log(tablero);
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let cantCol: number = 10; //Cantidad total de columnas
 let cantFil: number = 10; //Cantidad total de filas
-let cantMinas: number = 5; //Cantidad total de minas
+let cantMinas: number = 10; //Cantidad total de minas
 let tablero: any[][] = [[]]; //Array de objetos
 
 let divPrincipal = document.getElementById("principal");
@@ -9,9 +9,9 @@ let divPrincipal = document.getElementById("principal");
 class Caja {
     fil: number;
     col: number;
-    status: string;
+    status: number; //si es una mina el valor es -1, sino el numbero indica la cantidad de minas alrededor
 
-    constructor(fil: number, col: number, status: string) {
+    constructor(fil: number, col: number, status: number) {
         this.col = col;
         this.fil = fil;
         this.status = status;
@@ -26,7 +26,7 @@ function crearTablero(cantCol: number, cantFil: number) {
     for (let fil: number = 0; fil < cantFil; fil++) {
         tablero[fil] = [];
         for (let col: number = 0; col < cantCol; col++) {
-            tablero[fil][col] = new Caja(fil, col, "0");
+            tablero[fil][col] = new Caja(fil, col, 0);
         }
     }
 }
@@ -44,7 +44,51 @@ function previaTablero() {
     divPrincipal.innerHTML = imprimir;
 }
 
+//Función para crear minas aleatoreamente en filas y columnas
+//Recibe como argumento la cantidad de minas, que puede ser elegido desde la variable global cantMinas
+function crearMinas(cantMinas: number) {
+    while (cantMinas > 0) {
+        let filRand = Math.floor(Math.random() * cantFil);
+        let colRand = Math.floor(Math.random() * cantCol);
+        if (tablero[filRand][colRand].status != -1) {
+            tablero[filRand][colRand].status = -1;
+            cantMinas--;
+            //console.log(cantMinas);
+        }
+    }
+}
+
+//Función para asignar el número de minas vecinas
+//Para cada caja, verificamos si es una mina, y si no verificamos todas las vecinas
+//Si la vecina no es una mina o no esta fuera de tablero adicionamos en 1 el contador de status
+function asignarNumero() {
+    for (let fil: number = 0; fil < cantFil; fil++) {
+        for (let col: number = 0; col < cantCol; col++) {
+            if (tablero[fil][col].status === -1) {
+                for (let offCol = -1; offCol <= 1; offCol++) {
+                    for (let offFil = -1; offFil <= 1; offFil++) {
+                        //console.log(fil + offFil, col + offCol);
+
+                        if (
+                            //Debo verificar que no quede fuera del tablero y que no sea una mina (-1)
+                            fil + offFil >= 0 &&
+                            fil + offFil < cantFil &&
+                            col + offCol >= 0 &&
+                            col + offCol < cantCol &&
+                            tablero[fil + offFil][col + offCol].status !== -1
+                        ) {
+                            tablero[fil + offFil][col + offCol].status++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 crearTablero(cantCol, cantFil);
+crearMinas(cantMinas);
+asignarNumero();
 previaTablero();
 
-console.log(tablero);
+//console.log(tablero);
